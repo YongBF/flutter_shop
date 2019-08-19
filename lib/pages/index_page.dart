2987acf7e8be0f612589433448f1d7a6406b22c8
YongBF <1727132050@qq.com>
2019-08-19 18:02:00 +0800

    init

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class IndexPage extends StatefulWidget {
+  IndexPage({Key key}) : super(key: key);
+
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text('')
+    );
+  }
+}
